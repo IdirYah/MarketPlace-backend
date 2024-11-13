d58aeab0570aadd9b705b110ac73d07a53615213
IdirYah <mi_yahiaoui@esi.dz>
2024-11-14 00:21:43 +0100

    product controller added with success

--- controllers/Produit.controller.js
+++ controllers/Produit.controller.js
@@ -19,5 +19,84 @@ export const addProduct = async(req,res)=>{
         res.status(StatusCodes.CREATED).json({message:"Product added with success"}) 
     } catch (error) {
         console.log(error.message)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error:"Internal server error"})
+    }
+}
+
+export const updateProduct = async(req,res)=>{
+    try {
+        const {idProduit,domaine,nom,description,prix} = req.body
+        if(!idProduit){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"Product Id is required"})
+        }
+        const oldProduit = await Produit.findById(idProduit,)
+        if(!oldProduit){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"Product not found"})
+        }
+        oldProduit.domaine = domaine || oldProduit.domaine
+        oldProduit.nom = nom || oldProduit.nom
+        oldProduit.description = description || oldProduit.description
+        oldProduit.prix = prix || oldProduit.prix
+        await oldProduit.save()
+        res.status(StatusCodes.OK).json({message:"Product updated with sucess"})
+    } catch (error) {
+        console.log(error.message)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error:"Internal server error"})
+    }
+}
+
+export const getProductByName = async(req,res)=>{
+    try {
+        const {nom} = req.body
+        if(!nom){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"Name is required"})
+        }
+        const products = await Produit.find({nom})
+        if(products.length === 0){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"No product found with this name"})
+        }
+        res.status(StatusCodes.OK).json(products)
+    } catch (error) {
+        console.log(error.message)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error:"Internal server error"})
+    }
+}
+
+export const getProductByDomaine = async(req,res)=>{
+    try {
+        const {domaine} = req.body
+        if(!domaine){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"Domaine is required"})
+        }
+        const products = await Produit.find({domaine})
+        if(products.length === 0){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"No product found with this domaine"})
+        }
+        res.status(StatusCodes.OK).json(products)
+    } catch (error) {
+        console.log(error.message)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error:"Internal server error"})
+    }
+} 
+
+export const getProductByPrice = async(req,res)=>{
+    try {
+        const {prixMn,prixMx} = req.body
+        if(!prixMn || !prixMx){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"Prices are required"})
+        }
+        const prixMin = Number(prixMn)
+        const prixMax = Number(prixMx)
+        if (isNaN(prixMin) || isNaN(prixMax)) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: "Prices must be numbers" })
+        }
+        const products = await Produit.find({prix:{$gte:prixMin,$lte:prixMax}})
+        if(products.length === 0){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"No product found with these prices"})
+        }
+        res.status(StatusCodes.OK).json(products)
+    }catch(error){
+        console.log(error.message)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error:"Internal server error"})
     }
 }
--- routes/Produit.route.js
+++ routes/Produit.route.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { addProduct } from "../controllers/Produit.controller.js"
-import { authVendeur } from "../middlewares/Auth.middleware.js"
-import router from "./User.route.js"
+import { addProduct, getProductByDomaine, getProductByName, getProductByPrice, updateProduct } from "../controllers/Produit.controller.js"
+import { authVendeur,authAcheteur } from "../middlewares/Auth.middleware.js"
 
-const routes = express.Router()
+const router = express.Router()
 
 router.post("/addProduct",authVendeur,addProduct)
+router.put("/updateProduct",authVendeur,updateProduct)
+router.get("/getProductsByName",authAcheteur,getProductByName)
+router.get("/getProductsByDomaine",authAcheteur,getProductByDomaine)
+router.get("/getProductsByPrice",authAcheteur,getProductByPrice)
 
 export default router
