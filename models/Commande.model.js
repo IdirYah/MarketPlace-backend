568c0d4aa8c7dc037c2ed980772676b67b02d32d
IdirYah <mi_yahiaoui@esi.dz>
2024-12-04 09:37:01 +0100

    update commande model

--- models/Commande.model.js
+++ models/Commande.model.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 
 const commandeSchema = new mongoose.Schema({
     produitId:{type:mongoose.Schema.Types.ObjectId,ref:"Produit",required:true},
-    status:{type:Boolean,default:false},
+    acheteurId:{type:mongoose.Schema.Types.ObjectId,ref:"Acheteur",required:true},
+    isConfirmed:{type:Boolean,default:false},
+    isCancelled:{type:Boolean,default:false},
 },{timestamps:true})
 
 const Commande = mongoose.model("Commande",commandeSchema)
